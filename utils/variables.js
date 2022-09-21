76c5fef4bf8c94f948511dae611f5f306fe2b0cb
Bram <brampijper@gmail.com>
2022-09-21 16:04:11 +0200

    created a global variable module

---
+++ utils/variables.js
@@ -0,0 +1,5 @@
+const $form = document.querySelector('#form');
+const $noteTitle = document.querySelector('#note-title');
+const $formButtons = document.querySelector('#form-buttons');
+
+export { $form, $noteTitle, $formButtons }
