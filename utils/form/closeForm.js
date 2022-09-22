efd7ed3566bc6df697b5b6377872731f2932e83b
Bram <brampijper@gmail.com>
2022-09-22 10:54:30 +0200

    type fix

--- utils/form/closeForm.js
+++ utils/form/closeForm.js
@@ -2,6 +2,6 @@ import { $form, $noteTitle, $formButtons } from "../variables.js";
 
 export default function closeForm() {
     $form.classList.remove('form-open');
-    $noteTitle.style.dipslay = "none";
+    $noteTitle.style.display = "none";
     $formButtons.style.display = "none";
 }
