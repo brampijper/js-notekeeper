46f2c5763a6fd96bdde8591a3762c3dbdb733372
Bram <brampijper@gmail.com>
2022-09-22 11:53:46 +0200

    submit button now creates a new note

--- utils/addEventListeners.js
+++ utils/addEventListeners.js
@@ -1,7 +1,21 @@
+import { $noteText, $noteTitle } from './variables.js';
 import handleFormClick from './form/handleFormClick.js';
+import createNote from './createNote.js';
 
 export default function addEventListeners () {
     document.body.addEventListener('click', event => {
         handleFormClick(event);
+    })
+
+    document.body.addEventListener('submit', event => {
+        event.preventDefault(); //prevent auto refresh on submit.
+        const text = $noteText.value;
+        const title = $noteTitle.value;
+        const hasNote = text || title;
+        
+        if (hasNote) {
+            createNote( { text, title } )
+        }
+
     })
 }
