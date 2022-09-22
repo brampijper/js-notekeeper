301cef830a49f8460e4d8affca569cd2c5eaf486
Bram <brampijper@gmail.com>
2022-09-22 10:51:00 +0200

    typo fix

--- utils/form/openForm.js
+++ utils/form/openForm.js
@@ -2,6 +2,6 @@ import { $form, $noteTitle, $formButtons } from "../variables.js";
 
 export default function openForm() {
     $form.classList.add('form-open');
-    $noteTitle.style.dipslay = "block";
+    $noteTitle.style.display = "block"; //this one doesn't toggle from block to none
     $formButtons.style.display = "block";
 }
