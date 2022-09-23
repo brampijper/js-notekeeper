87de9231ae57a904bbc2937559de5534522d1e3c
Bram <brampijper@gmail.com>
2022-09-23 13:29:08 +0200

    removed unused render method

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ import addEventListeners from './utils/addEventListeners.js';
 class App {
     constructor() { //in the constructor you can run any function you like.
         addEventListeners();        
-        this.render();
+        // this.render();
     }
 
-    render() {
-        document.getElementById('root').append
-        (`
-            hello world
-        `)
-        }
+    // render() {
+    //     document.getElementById('root')
+    // }
 }
 
 new App();
