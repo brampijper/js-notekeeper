4e349c4ec65cd6a62fe0f8afc344363a58d701a3
Bram <brampijper@gmail.com>
2022-09-23 16:48:52 +0200

    user can now close the modal when editing a note

--- utils/addEventListeners.js
+++ utils/addEventListeners.js
@@ -1,11 +1,16 @@
-import { $noteText, $noteTitle, $formCloseButton } from './variables.js';
+import { $noteText, $noteTitle, $formCloseButton, $modalCloseButton } from './variables.js';
 import handleFormClick from '../form/handleFormClick.js';
 import createNote from '../notes/createNote.js';
 import closeForm from "../form/closeForm.js";
+import selectNote from '../notes/selectNote.js';
+import closeModal from '../modal/closeModal.js';
+import openModal from '../modal/openModal.js';
 
 export default function addEventListeners () {
     document.body.addEventListener('click', event => {
         handleFormClick(event);
+        selectNote(event);
+        openModal(event);
     })
 
     document.body.addEventListener('submit', event => {
@@ -23,5 +28,10 @@ export default function addEventListeners () {
     $formCloseButton.addEventListener('click', event => {
         event.stopPropagation();
         closeForm();
+    })
+
+    $modalCloseButton.addEventListener('click', event => {
+        event.stopPropagation();
+        closeModal(event);
     })
 }
