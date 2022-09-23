d9f7bbc78785b5542e65ac049fd4c304201f66a4
Bram <brampijper@gmail.com>
2022-09-23 13:16:58 +0200

    Notes are now stored if the user clicks on blank space

--- form/handleFormClick.js
+++ form/handleFormClick.js
@@ -1,12 +1,19 @@
-import { $form } from "../utils/variables.js"
+import { $form, $noteText, $noteTitle } from "../utils/variables.js"
 import openForm from "./openForm.js";
 import closeForm from "./closeForm.js";
+import createNote from "../notes/createNote.js";
 
 export default function handleFormClick(e) {
     const isFormClicked = $form.contains(e.target);
 
+    const text = $noteText.value;
+    const title = $noteTitle.value;
+    const hasNote = text || title;
+
     if (isFormClicked) {
         openForm();
+    } else if (hasNote) {
+        createNote({title, text})
     } else {
         closeForm();
     }
