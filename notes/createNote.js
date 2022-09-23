a720ebcfd1bc6cbe217bad3a14e34b9f61bb04d7
Bram <brampijper@gmail.com>
2022-09-23 11:58:38 +0200

    moved to note folder

--- notes/createNote.js
+++ notes/createNote.js
@@ -1,4 +1,5 @@
 import store from '../store.js';
+import displayNote from './displayNote.js';
 
 export default function createNote({ title, text }) {
     const { notes } = store.getState();
@@ -6,10 +7,12 @@ export default function createNote({ title, text }) {
         title: title,
         text: text,
         color: 'white',
-        id: notes.length > 0 ? notes[notes.length - 1].id + 1 : 1 //this needs explaining
+        id: notes.length > 0 ? notes[notes.length - 1].id + 1 : 1 // Position of first element in array starts at 0, but .length starts at 1: that's why -1 and +1 to match the ID with the corresponding element position. 
     };
 
     const action = { type: "CREATE_NOTE", payload: { newNote } };
     store.dispatch(action);
     console.log(store.getState());
+    
+    displayNote();
 }
