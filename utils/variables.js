016df961d19290818c54d62d0d068bd90cab9049
Bram <brampijper@gmail.com>
2022-09-23 13:30:09 +0200

    add close button var

--- utils/variables.js
+++ utils/variables.js
@@ -4,6 +4,7 @@ const $noteText = document.querySelector('#note-text');
 const $formButtons = document.querySelector('#form-buttons');
 const $placeholder = document.querySelector('#placeholder');
 const $notes = document.querySelector('#notes');
+const $formCloseButton = document.querySelector('#form-close-button');
 
 
-export { $form, $noteTitle, $noteText, $formButtons, $placeholder, $notes }
+export { $form, $noteTitle, $noteText, $formButtons, $placeholder, $notes, $formCloseButton }
