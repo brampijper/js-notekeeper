057c989732c19e32d4392dd25f351f45eb6b4430
Bram <brampijper@gmail.com>
2022-09-23 16:48:29 +0200

    added more variables

--- utils/variables.js
+++ utils/variables.js
@@ -1,10 +1,20 @@
 const $form = document.querySelector('#form');
-const $noteTitle = document.querySelector('#note-title');
-const $noteText = document.querySelector('#note-text');
+const $formCloseButton = document.querySelector('#form-close-button');
 const $formButtons = document.querySelector('#form-buttons');
 const $placeholder = document.querySelector('#placeholder');
+
+const $noteTitle = document.querySelector('#note-title');
+const $noteText = document.querySelector('#note-text');
 const $notes = document.querySelector('#notes');
-const $formCloseButton = document.querySelector('#form-close-button');
 
+const $modal = document.querySelector('.modal');
+const $modalTitle = document.querySelector('.modal-title');
+const $modalText = document.querySelector('.modal-text');
+const $modalCloseButton = document.querySelector('.modal-close-button');
 
-export { $form, $noteTitle, $noteText, $formButtons, $placeholder, $notes, $formCloseButton }
+export { 
+    $form, $formButtons, $formCloseButton, 
+    $noteTitle, $noteText, $notes, 
+    $modal, $modalTitle, $modalText, $modalCloseButton,
+    $placeholder,
+};
