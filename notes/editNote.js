505933d690932fa6194f92ab174a7f7d0af165b9
Bram <brampijper@gmail.com>
2022-09-23 16:49:08 +0200

    user can edit a note by typing in the modal

---
+++ notes/editNote.js
@@ -0,0 +1,18 @@
+import store from "../store.js";
+import { $modalText, $modalTitle } from "../utils/variables.js";
+import displayNote from "./displayNote.js";
+
+export default function editNote() {
+    const title = $modalTitle.value;
+    const text = $modalText.value;
+
+    const newNote = {
+        title,
+        text
+    }
+    
+    const action = { type: "EDIT_NOTE", payload: { newNote }}
+    store.dispatch(action);
+
+    displayNote();
+}
