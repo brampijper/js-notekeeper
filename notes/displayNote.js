6ff2ed6461bc2fcdc3dee6e72e8fb6e638ba89b1
Bram <brampijper@gmail.com>
2022-09-23 11:58:15 +0200

    A created note is now displayed

---
+++ notes/displayNote.js
@@ -0,0 +1,21 @@
+import store from '../store.js';
+import { $placeholder, $notes } from '../utils/variables.js';
+
+export default function displayNote() {
+    const { notes } = store.getState();
+    const hasNotes = notes.length > 0;
+    $placeholder.style.display = hasNotes ? 'none' : 'flex';
+
+    $notes.innerHTML = notes.map( note => `
+        <div style="background: ${note.color};" class="note">
+            <div class="${note.title && 'note-title'}">${note.title}</div>
+            <div class="note-text">${note.text}</div>
+            <div class="toolbar-container">
+                <div class="toolbar">
+                    <img class="toolbar-color" src="https://icon.now.sh/palette">
+                    <img class="toolbar-delete" src="https://icon.now.sh/delete">
+                </div>
+            </div>
+        </div>
+    `)
+}
