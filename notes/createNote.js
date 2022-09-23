cabfaa09258473ad12763674f377a69ed487728b
Bram <brampijper@gmail.com>
2022-09-23 13:15:43 +0200

    code optimalisation: using object shorthand

--- notes/createNote.js
+++ notes/createNote.js
@@ -4,8 +4,8 @@ import displayNote from './displayNote.js';
 export default function createNote({ title, text }) {
     const { notes } = store.getState();
     const newNote = {
-        title: title,
-        text: text,
+        title,
+        text,
         color: 'white',
         id: notes.length > 0 ? notes[notes.length - 1].id + 1 : 1 // Position of first element in array starts at 0, but .length starts at 1: that's why -1 and +1 to match the ID with the corresponding element position. 
     };
