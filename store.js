7232614415e99d6470ed9457985d7c826d318262
Bram <brampijper@gmail.com>
2022-09-25 15:18:59 +0200

    user can now select and edit a note

--- store.js
+++ store.js
@@ -10,15 +10,35 @@ function createStore(reducer) {
 }
 
 const initialState = {
-    notes: []
+    notes: [],
+    title: '',
+    text: '',
+    id: ''
 }
 
 function noteReducer(state = initialState, action) {
     switch (action.type) {
-        case "CREATE_NOTE":
+        case "CREATE_NOTE": {
             const createdNote = action.payload.newNote;
             const notes = [...state.notes, createdNote];
+            return { notes }
+        }
+        case "SELECT_NOTE": {
+            const selectedNote = action.payload.selectedNote;
+            const title = selectedNote.title;
+            const text = selectedNote.text;
+            const id = selectedNote.id; 
+            return { ...state, title, text, id, } // always return something to update state. This is object shorthand for (initialState.title:title)
+        }
+        case "EDIT_NOTE": {
+            const { title, text } = action.payload.newNote;
+            const notes = state.notes.map( (note) => {
+                note.id === Number(state.id)
+                ? { ...note, title, text }
+                : note; 
+            })
             return { notes };
+        }
         default: 
             return state;
     }
