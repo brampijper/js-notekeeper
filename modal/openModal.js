59c8a0e5d74740da533ab845fc2bc997a37b350a
Bram <brampijper@gmail.com>
2022-09-26 19:17:37 +0200

    bug fix: modal stays closeed when clicking on the trash icon

--- modal/openModal.js
+++ modal/openModal.js
@@ -2,8 +2,11 @@ import { $modal, $modalText, $modalTitle } from "../utils/variables.js";
 import store from '../store.js';
 
 export default function openModal (event) {
-    const { title, text } = store.getState();
+    if(event.target.matches('.toolbar-delete')) return;
+
     if(event.target.closest('.note')) { 
+        const { title, text } = store.getState();
+        
         $modal.classList.toggle('open-modal');
         $modalTitle.value = title;
         $modalText.value = text;
